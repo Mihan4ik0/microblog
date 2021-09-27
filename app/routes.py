de086fd2f0ffd0f5861cc6f94a353b2cb463639c
Mihan4ik0 <miha23112003@gmail.com>
2021-09-27 19:25:33 +0300

    Version 3.0

--- app/routes.py
+++ app/routes.py
@@ -1,23 +1,35 @@
-from flask import render_template
+from flask import render_template, flash, redirect, url_for
 from app import app
+from app.forms import LoginForm
 
 
+# Создаём функцию для обработки главных страниц
 @app.route('/')
 @app.route('/index')
 def index():
-        user = {'username': 'Misha'}
-        posts = [
-                {
-                        'author': {'username': 'John'},
-                        'body': 'Beautiful day in Portland!'
-                },
-                {
-                        'author': {'username': 'Susan'},
-                        'body': 'The avengers movie was so cool'
-                },
-                {
-                        'author': {'username': 'Ипполит'},
-                        'body': 'Какая гадость эта ваша заливная рыба!'
-                }
-        ]
-        return render_template('index.html', title='Home', user=user, posts=posts)
+    user = {'username': 'Misha'}
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The avengers movie was so cool'
+        },
+        {
+            'author': {'username': 'Ипполит'},
+            'body': 'Какая гадость эта ваша заливная рыба!'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)  # То что возвращаем по итогу
+
+
+# Создаём функцию для авторизации пользователей
+@app.route('/login', methods=['GET', 'POST'])  # methods принимает запросы 'GET' и 'POST'
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():  # Пропускается при 'GET',так как form.validate_on_submit()=False
+        flash('Login requested for user {}, remember_me={}'.format(form.username.data, form.remember_me.data))  # Вывод сообщения пользователю
+        return redirect(url_for('login'))  # Перенаправление на страницу '/index'
+    return render_template('login.html', title='Sign in', form=form)
